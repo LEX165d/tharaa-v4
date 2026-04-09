91746677617b1f1caad2f3b9cd92c0822392a204
LEX165d <stfsafar@gmail.com>
2026-04-09 16:38:55 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.yourname.myapp',
+  appName: 'myapp',
+  webDir: 'dist'
+};
+
+export default config;
